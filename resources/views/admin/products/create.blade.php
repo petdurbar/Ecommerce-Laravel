c746ff0b01e91294816ba15216c886d21b693240
DevChandraShrestha <dev@softsaro.com>
2024-03-18 20:56:24 +0545

    removed comissions

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -38,24 +38,6 @@
                             </div>
                         </div>
 
-                        {{-- Product order --}}
-                        {{-- <div class="mt-2">
-                            <label class="text-sm font-semibold w-full " htmlFor="">
-                                Product order
-                            </label>
-                            <div>
-                                <input
-                                    class="text-xs border border-gray-300 p-3 rounded mt-3 focus:border-[#7065d4] hover:border-[#7065d4] w-full"
-                                    name="product_order" placeholder="Type product order here" type="text"
-                                    value="{{ old('product_order') }}" />
-                                @error('product_order')
-                                    <div class="invalid-feedback text-red-400 text-sm" style="display: block;">
-                                        * {{ $message }}
-                                    </div>
-                                @enderror
-                            </div>
-                        </div> --}}
-
                         {{-- MRP price --}}
                         <div class="mt-2">
                             <label class="text-sm font-semibold w-full" htmlFor="">MRP Price</label>
@@ -121,26 +103,6 @@
                                 @enderror
                             </div>
                         </div>
-
-                        {{-- cut price --}}
-                        {{-- <div class="mt-2">
-                            <label class="text-sm font-semibold w-full " htmlFor="">
-                                Cutover Price
-
-                            </label>
-                            <div>
-                                <input
-                                    class="text-xs border border-gray-300 p-3 rounded mt-3 focus:border-[#7065d4] hover:border-[#7065d4] w-full"
-                                    name="cutoff_price" placeholder="Type product cut price here" type="text"
-                                    value="{{ old('cutoff_price') }}" />
-                                @error('cutoff_price')
-                                    <div class="invalid-feedback text-red-400 text-sm" style="display: block;">
-                                        * {{ $message }}
-                                    </div>
-                                @enderror
-                            </div>
-                        </div> --}}
-
 
                         {{-- image --}}
                         <div class="mt-3">
@@ -217,145 +179,6 @@
                 </div>
                 <div class="w-5/12  ml-3 ">
 
-                    {{-- commission form --}}
-                    <div class=" border-b bg-white p-3 rounded shadow-lg">
-                        <div>
-                            <label class="text-md font-semibold">Margin : </label>
-                            Rs. <input id="margin" type="text" name="margin" value="0" disabled
-                                class=" bg-white" />
-                        </div>
-                        <input id="margin2" type="hidden" name="margin" value="0" class=" bg-white" />
-
-                        @error('margin')
-                            <div class="invalid-feedback text-red-400 text-sm" style="display: block;">
-                                * {{ $message }}
-                            </div>
-                        @enderror
-
-                        {{-- Incentive commission --}}
-                        <div class="mt-2">
-                            <label class="text-sm font-semibold w-full" htmlFor="">
-                                Incentive Commission
-                                <span class="italic text-gray-500">(in percentage)</span>
-                            </label>
-                            <div>
-                                <input id="incentive_commission_percentage"
-                                    class="text-xs border border-gray-300 p-3 rounded mt-3 focus:border-[#7065d4] hover:border-[#7065d4] w-full"
-                                    name="incentive_commission_percentage" placeholder="Type incentive commission here"
-                                    type="text" value="{{ old('incentive_commission_percentage') }}" />
-                                @error('incentive_commission_percentage')
-                                    <div class="invalid-feedback text-red-400 text-sm" style="display: block;">
-                                        * {{ $message }}
-                                    </div>
-                                @enderror
-                            </div>
-                        </div>
-
-                        {{-- Referal commission --}}
-                        {{-- <div class="mt-2">
-                            <label class="text-sm font-semibold w-full" htmlFor="">
-                                Referal Commission
-                                <span class="italic text-gray-500">(in percentage)</span>
-                            </label>
-                            <div>
-                                <input id="referal_commission_percentage"
-                                    class="text-xs border border-gray-300 p-3 rounded mt-3 focus:border-[#7065d4] hover:border-[#7065d4] w-full"
-                                    name="referal_commission_percentage" placeholder="Type referal commission here" type="text"
-                                    value="{{ old('referal_commission_percentage') }}" />
-                                @error('referal_commission_percentage')
-                                    <div class="invalid-feedback text-red-400 text-sm" style="display: block;">
-                                        * {{ $message }}
-                                    </div>
-                                @enderror
-                            </div>
-                        </div> --}}
-
-                        {{-- Affiliate commission --}}
-                        <div class="mt-2">
-                            <label class="text-sm font-semibold w-full" htmlFor="">
-                                Affiliate Commission
-                                <span class="italic text-gray-500">(in percentage)</span>
-                            </label>
-                            <div>
-                                <input id="affiliate_commission_percentage"
-                                    class="text-xs border border-gray-300 p-3 rounded mt-3 focus:border-[#7065d4] hover:border-[#7065d4] w-full"
-                                    name="affiliate_commission_percentage" placeholder="Type Affiliate commission here"
-                                    type="text" value="{{ old('affiliate_commission_percentage') }}" />
-                                @error('affiliate_commission_percentage')
-                                    <div class="invalid-feedback text-red-400 text-sm" style="display: block;">
-                                        * {{ $message }}
-                                    </div>
-                                @enderror
-                            </div>
-                        </div>
-                    </div>
-
-                    {{-- commission calculation --}}
-                    <div class="mt-3 bg-white rounded p-3 shadow-lg">
-
-                        {{-- Incentive commission --}}
-                        <div>
-                            <div>
-                                <label class="text-md font-semibold">Incentive Commission : </label>
-                                Rs. <input id="incentive_commission_amount" type="text"
-                                    name="incentive_commission_amount" value="0" disabled class=" bg-white mb-2" />
-                            </div>
-                            @error('incentive_commission_amount')
-                                <div class="invalid-feedback text-red-400 text-sm" style="display: block;">
-                                    * {{ $message }}
-                                </div>
-                            @enderror
-                            <input id="incentive_commission_amount2" type="hidden" name="incentive_commission_amount"
-                                value="0" class=" bg-white mb-2" />
-                        </div>
-
-                        {{-- Referal commission --}}
-                        {{-- <div>
-                            <div>
-                                <label class="text-md font-semibold">Referal Commission : </label>
-                                Rs. <input id="referal_commission_amount" type="text" name="referal_commission_amount"
-                                    value="0" disabled class=" bg-white mb-2" />
-                            </div>
-                            @error('referal_commission_amount')
-                                <div class="invalid-feedback text-red-400 text-sm" style="display: block;">
-                                    * {{ $message }}
-                                </div>
-                            @enderror
-                            <input id="referal_commission_amount2" type="hidden" name="referal_commission_amount"
-                                value="0" class=" bg-white mb-2" />
-                        </div> --}}
-
-                        {{-- Affiliate commission --}}
-                        <div>
-                            <div>
-                                <label class="text-md font-semibold">Affiliate Commission : </label>
-                                Rs. <input id="affiliate_commission_amount" type="text"
-                                    name="affiliate_commission_amount" value="0" disabled class=" bg-white mb-2" />
-                            </div>
-                            @error('affiliate_commission_amount')
-                                <div class="invalid-feedback text-red-400 text-sm" style="display: block;">
-                                    * {{ $message }}
-                                </div>
-                            @enderror
-                            <input id="affiliate_commission_amount2" type="hidden" name="affiliate_commission_amount"
-                                value="0" class=" bg-white mb-2" />
-                        </div>
-                        {{--  Discount Amount --}}
-                        <div>
-                            <div>
-                                <label class="text-md font-semibold">Discount Amount : </label>
-                                Rs. <input id="discount_amount2" type="text" name="discount_amount" value="0"
-                                    disabled class=" bg-white mb-2" />
-                            </div>
-                            @error('discount_amount')
-                                <div class="invalid-feedback text-red-400 text-sm" style="display: block;">
-                                    * {{ $message }}
-                                </div>
-                            @enderror
-                            <input id="discount_amount" type="hidden" name="discount_amount" value="0"
-                                class=" bg-white mb-2" />
-                        </div>
-                    </div>
                     {{-- attributes --}}
                     <div class="mt-3 bg-white rounded p-3 shadow-lg">
                         <div class="mt-4 ">
